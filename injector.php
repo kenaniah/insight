3ce7d11d776eab5e6b2336bfb2e81a1ea8ee1465
Kenaniah Cerny <kenaniah@gmail.com>
2011-10-24 12:36:30 -0700

    Added registry and injector classes.

---
+++ injector.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Dependency injection class
+ *
+ * Injector is a dependency injection wrapper that allows one to
+ * get and set resources using PHP's magic get/set functionality.
+ *
+ * Injectors only accept objects and/or closures as valid properties.
+ * Attempting to set a property to anything else will result in an error.
+ */
+class Injector {
+
+	/**
+	 * List of properties for this injector
+	 * @var array of objects and/or closures
+	 */
+	protected $objects = array();
+
+	/**
+	 * Set closure to create a given object, or set given object directly
+	 * @param string $name
+	 * @param mixed $closure
+	 */
+	public function __set($name, $closure) {
+
+		if(!is_object($closure)):
+			user_error("Only objects / closures may be passed to inejectors");
+		endif;
+
+		$this->objects[$name] = $closure;
+
+	}
+
+	/**
+	 * Retrieve object of given name (or returns the call result if the object is a Closure)
+	 * @param string $name
+	 */
+	public function __get($name) {
+
+		if(!isset($this->objects[$name])):
+			user_error("Object $name does not exist.");
+		endif;
+
+		//Either call the closure or return the object
+		return $this->objects[$name] instanceof Closure ? $this->objects[$name]($this) : $this->objects[$name];
+
+	}
+
+	/**
+	 * Create a new object with paremeters from the injector
+	 * @param string $name
+	 * @param array(mixed) $arguments
+	 */
+	public function __call($name, array $arguments) {
+
+		if(!isset($this->objects[$name]))
+			user_error("Object $name does not exist.");
+
+		if(!is_callable($this->objects[$name]))
+			user_error('Cannot instantiate non-closure object with arguments.');
+
+		return $this->objects[$name]( $this, $arguments );
+
+	}
+
+}
---
+++ registry.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Registry class used for dependency injection and tracking of shared variables
+ *
+ * While this class may not be instantiated, it may be cloned to allow for non-global modifications
+ * before an injection takes place.
+ *
+ * Can be used to store:
+ *   - Objects directly
+ *   - Names of objects to be instantiated upon retrieval
+ *   - Closures that construct objects upon retrieval
+ *   - Constructing closures wrapped by $this->asShared() to ensure a global instance upon retrieval
+ *
+ * @author Kenaniah Cerny <kenaniah@gmail.com>
+ * @version 1.0.2
+ * @license http://creativecommons.org/licenses/by/3.0/us/
+ * @copyright Copyright (c) 2009, Kenaniah Cerny
+ */
+class Registry {
+
+	protected $values = array();
+
+	final public static function getInstance() {
+
+		static $instance;
+
+		if($instance) return $instance;
+
+		return $instance = new self;
+
+	}
+
+	function __set($id, $value){
+
+		$this->values[$id] = $value;
+
+	}
+
+	static function set($id, $value){
+
+		$instance = self::getInstance();
+		$instance->__set($id, $value);
+
+	}
+
+	/**
+	 * Returns the value (or runs and returns if a closure)
+	 */
+	function __get($id){
+
+		if(!isset($this->values[$id])){
+			throw new InvalidArgumentException(sprintf('Value "%s" is not defined.', $id));
+		}
+
+		if($this->values[$id] instanceof Closure){
+			return $this->values[$id]($this);
+		}else{
+			return $this->values[$id];
+		}
+
+	}
+
+	static function get($id){
+
+		$instance = self::getInstance();
+		return $instance->__get($id);
+
+	}
+
+	function __isset($id){
+
+		return array_key_exists($id, $this->values);
+
+	}
+
+	function __unset($id){
+
+		unset($this->values[$id]);
+
+	}
+
+}
